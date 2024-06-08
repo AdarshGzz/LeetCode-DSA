bc491791382b2b9bb9ade548d0dd36786f905ed1
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2024-06-08 18:08:54 +0530

    Time: 23 ms (88.33%), Space: 15.9 MB (56.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
-    void handleIsland(vector<vector<char>>& matrix, int x, int y, int r,
-                      int c) {
-        // Check if the current cell is out of bounds or not an island part
-        // ('1')
-        if (x < 0 || x >= r || y < 0 || y >= c || matrix[x][y] != '1') {
-            return;
-        }
-        // Mark the current cell as visited by changing it to '2'
-        matrix[x][y] = '2';
-
-        // Recursively handle the neighboring cells (up, down, left, right)
-        handleIsland(matrix, x + 1, y, r, c); // Down
-        handleIsland(matrix, x, y + 1, r, c); // Right
-        handleIsland(matrix, x - 1, y, r, c); // Up
-        handleIsland(matrix, x, y - 1, r, c); // Left
+    void handleIsland(vector<vector<char>>& grid,int x,int y,int r,int c){
+        if(x<0 || y<0 || x>=r || y>=c || grid[x][y]!='1') return;
+        grid[x][y] = '2';
+        handleIsland(grid,x+1,y,r,c);
+        handleIsland(grid,x,y+1,r,c);
+        handleIsland(grid,x-1,y,r,c);
+        handleIsland(grid,x,y-1,r,c); 
     }
-
-    int numIslands(
-        vector<vector<char>>& grid) { // Corrected function name to numIslands
-        int row = grid.size();
-        if (row == 0) {
-            return 0;
-        }
-        int col = grid[0].size();
-
-        int Islands = 0;
-
-        // Iterate through each cell in the grid
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                // If the cell is an unvisited island part ('1'), handle the
-                // island
-                if (grid[i][j] == '1') {
-                    handleIsland(grid, i, j, row, col);
-                    Islands++;
+    int numIslands(vector<vector<char>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        if(r==0) return 0;
+        int island = 0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]=='1'){
+                    handleIsland(grid,i,j,r,c);
+                    island++;
                 }
             }
         }
-        return Islands;
+        return island;
     }
 };
