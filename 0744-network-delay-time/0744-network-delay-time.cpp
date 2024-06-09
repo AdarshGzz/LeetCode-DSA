b600ac5e1a5362df5a8ca078c8c2a41e315aa754
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2024-06-09 13:31:03 +0530

    Time: 103 ms (15.28%), Space: 47.2 MB (21.22%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int, vector<pair<int, int>>> graph;
-    for (auto& i : times) {
-        int sn = i[0];
-        int tn = i[1];
-        int t = i[2];
-        graph[sn].push_back({tn, t});
-    }
-
-    vector<int> distance(n + 1, INT_MAX);
-    distance[k] = 0;
-
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
-    minHeap.push({0, k});
-
-    while (!minHeap.empty()) {
-        int cost = minHeap.top().first;
-        int currNode = minHeap.top().second;
-        minHeap.pop();
-
-        if (cost > distance[currNode]) {
-            continue;
+        unordered_map<int,vector<pair<int,int>>> graph;
+        for(auto i: times){
+            int sn = i[0];
+            int tn = i[1];
+            int t = i[2];
+            graph[sn].push_back({tn,t});
         }
 
-        for (auto& g : graph[currNode]) {
-            int tn = g.first;
-            int t = g.second;
-            if (distance[tn] > cost + t) {
-                distance[tn] = cost + t;
-                minHeap.push({distance[tn], tn});
+        vector<int> distance(n+1,INT_MAX);
+        distance[k] = 0;
+
+        priority_queue <pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minHeap;
+        minHeap.push({0,k});
+
+        while(!minHeap.empty()){
+            int currDistance = minHeap.top().first;
+            int currNode = minHeap.top().second;
+            minHeap.pop();
+
+            if(currDistance>distance[currNode]){
+                continue;
+            }
+            for(auto g:graph[currNode]){
+                int tn = g.first;
+                int t = g.second;
+                if(distance[tn] > currDistance + t){
+                    distance[tn] = currDistance + t;
+                    minHeap.push({distance[tn],tn});
+                } 
             }
         }
-    }
 
-    int maxDist = *max_element(distance.begin() + 1, distance.end());
-    return maxDist == INT_MAX ? -1 : maxDist;
+        int maxDist = *max_element(distance.begin()+1,distance.end());
+        return maxDist == INT_MAX? -1 : maxDist;
     }
 };
